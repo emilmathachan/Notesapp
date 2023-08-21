c56b976c1f0fa4ec9ab6beffea1dfcea1f05e954
emilmathachan <emilmathachan18@gmail.com>
2023-08-21 18:34:09 +0530

    adding views

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
 
 auth = Blueprint('auth',__name__)
+
+@auth.route('/login')
+def login():
+    return"<p>Login</P>"
+
+@auth.route('/logout')
+def logout():
+    return"<p>Logout/P>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return"<p>Sign up</P>"
+
